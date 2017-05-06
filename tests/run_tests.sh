2967c38144f1c3b125064fb889ec8c0ed5e86c6f
David Duenn <daduenn@umich.edu>
2017-05-05 21:26:07 -0500

    test: diff side-by-side

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,5 +1,7 @@
+echo "Running 'exit' test"
 ./MY_BANK < ./tests/input/exit.txt > ./tests/output/exit.txt
-diff ./tests/output/exit.txt ./tests/answers/exit.txt
+diff --side-by-side ./tests/output/exit.txt ./tests/answers/exit.txt
 
+echo "Running 'accounts' test"
 ./MY_BANK < ./tests/input/accounts.txt > ./tests/output/accounts.txt
-diff ./tests/output/accounts.txt ./tests/answers/accounts.txt
+diff --side-by-side ./tests/output/accounts.txt ./tests/answers/accounts.txt
