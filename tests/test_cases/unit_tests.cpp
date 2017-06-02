e666a2e4b0b362aa5d5ede49ea705dd5102c0b20
David Duenn <daduenn@umich.edu>
2017-06-01 19:16:07 -0500

    Add test of withdrawing money from unit tests

--- tests/test_cases/unit_tests.cpp
+++ tests/test_cases/unit_tests.cpp
@@ -15,9 +15,15 @@ int main() {
   cout << test_bank_inst.account_exists(jw) << endl;
   cout << test_bank_inst.account_exists(pf) << endl;
 
-  test_bank_inst.transact(dd,  23);
-  test_bank_inst.transact(jw, 630);
-  test_bank_inst.transact(pf, 204);
+  test_bank_inst.transact(dd,  23) ? cout << "Tranx failed" << endl : cout << "Trnx succeeded" << endl;
+  test_bank_inst.transact(jw, 130) ? cout << "Tranx failed" << endl : cout << "Trnx succeeded" << endl;
+  test_bank_inst.transact(pf,  24) ? cout << "Tranx failed" << endl : cout << "Trnx succeeded" << endl;
+
+  test_bank_inst.print_bank();
+
+  test_bank_inst.transact(dd, -23) ? cout << "Tranx failed" << endl : cout << "Trnx succeeded" << endl;
+  test_bank_inst.transact(jw, 200) ? cout << "Tranx failed" << endl : cout << "Trnx succeeded" << endl;
+  test_bank_inst.transact(pf, -80) ? cout << "Tranx failed" << endl : cout << "Trnx succeeded" << endl;
 
   test_bank_inst.print_bank();
 
