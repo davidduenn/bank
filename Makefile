e0201ce34d7d83ada298394831c4fb59adc2c1b6
David Duenn <davidduenn@gmail.com>
2018-01-24 17:20:03 -0500

    update unit_tests

--- Makefile
+++ Makefile
@@ -40,9 +40,9 @@ test_input:	MY_BANK
 	sed '/id\( is\)\?:/d' -i ./tests/output/input.txt
 	-diff  ./tests/output/input.txt ./tests/answers/input.txt
 
-test_units:	helpers.o bank.o account.o
+test_units:	helpers.o bank.o member.o account.o
 	if [ ! -f ./tests/output/units.txt ]; then touch ./tests/output/units.txt; fi
-	g++ -g -std=c++11 tests/test_cases/unit_tests.cpp bank.o account.o -o cpp_test_units
+	g++ -g -std=c++11 tests/test_cases/unit_tests.cpp bank.o member.o account.o -o cpp_test_units
 	./cpp_test_units > ./tests/output/units.txt
 	sed '/id\( is\)\?:/d' -i ./tests/output/units.txt
 	-diff  ./tests/output/units.txt ./tests/answers/units.txt
--- tests/test_cases/unit_tests.cpp
+++ tests/test_cases/unit_tests.cpp
@@ -7,10 +7,13 @@ using namespace std;
 int main() {
   bank test_bank_inst;
 
-  int dd = test_bank_inst.create_account("David", "Duenn");
-  int jw = test_bank_inst.create_account("John", "Wayne");
-  int pf = test_bank_inst.create_account("Pope", "Francis");
+  //int dd = test_bank_inst.add_member("David Duenn", 54, '$');
+  for(int i=0; i<8000; i++) {
+    int jw = test_bank_inst.add_member("John Wayne", 64, '$');
+  }
+  //int pf = test_bank_inst.add_member("Pope Francis", 44, '$');
 
+  /*
   test_bank_inst.account_exists(dd) ? cout << "account exists" << endl : cout << "no such account" << endl;
   test_bank_inst.account_exists(jw) ? cout << "account exists" << endl : cout << "no such account" << endl;
   test_bank_inst.account_exists(pf) ? cout << "account exists" << endl : cout << "no such account" << endl;
@@ -25,6 +28,7 @@ int main() {
   test_bank_inst.transact(dd, -23) ? cout << "Tranx failed" << endl : cout << "Trnx succeeded" << endl;
   test_bank_inst.transact(jw, 200) ? cout << "Tranx failed" << endl : cout << "Trnx succeeded" << endl;
   test_bank_inst.transact(pf, -80) ? cout << "Tranx failed" << endl : cout << "Trnx succeeded" << endl;
+  */
 
   test_bank_inst.print_bank();
 
